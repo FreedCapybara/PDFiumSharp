d4a9a674d3a4a2e36494f6cb990746a26822985a
atemple <templeandrew@outlook.com>
2020-03-02 13:34:44 -0500

    Apply annotations.h

---
+++ PDFiumSharp/AnnotationColorTypes.cs
@@ -0,0 +1,23 @@
+﻿#region Copyright and License
+/*
+This file is part of PDFiumSharp, a wrapper around the PDFium library for the .NET framework.
+Copyright (C) 2017 Tobias Meyer
+License: Microsoft Reciprocal License (MS-RL)
+*/
+# endregion
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PDFiumSharp
+{
+	/// <summary>
+	/// PDF Action Types
+	/// </summary>
+	public enum AnnotationColorTypes : uint
+	{
+		Color = 0,
+
+		InteriorColor
+	}
+}
---
+++ PDFiumSharp/Types/FS_POINTF.cs
@@ -0,0 +1,25 @@
+﻿#region Copyright and License
+/*
+This file is part of PDFiumSharp, a wrapper around the PDFium library for the .NET framework.
+Copyright (C) 2017 Tobias Meyer
+License: Microsoft Reciprocal License (MS-RL)
+*/
+#endregion
+using System;
+using System.Runtime.InteropServices;
+
+namespace PDFiumSharp.Types
+{
+	[StructLayout(LayoutKind.Sequential)]
+	public struct FS_POINTF
+    {
+		public float X { get; }
+		public float Y { get; }
+
+		public FS_POINTF(float x, float y)
+		{
+			X = x;
+			Y = y;
+		}
+	}
+}
